2d0dc5dc9f57c36f1751ce57b70573d93224362e
Muhammad Zamroni <halo@matriphe.com>
2020-10-31 16:46:46 +0100

    Force all drivers to md5

--- src/Md5HasherManager.php
+++ src/Md5HasherManager.php
@@ -7,6 +7,8 @@ use Illuminate\Hashing\HashManager;
 class Md5HasherManager extends HashManager
 {
 	/**
+	 * Create an instance of the MD5 hash Driver.
+	 *
 	 * @return Md5Hasher
 	 */
 	public function createMd5Driver()
@@ -15,7 +17,37 @@ class Md5HasherManager extends HashManager
 	}
 
 	/**
-	 * Get the default driver name.
+	 * Replace the Bcrypt hash Driver with MD5 Driver.
+	 *
+	 * @return Md5Hasher
+	 */
+	public function createBcryptDriver()
+	{
+		return $this->createMd5Driver();
+	}
+
+	/**
+	 * Replace the Argon hash Driver with MD5 Driver.
+	 *
+	 * @return Md5Hasher
+	 */
+	public function createArgonDriver()
+	{
+		return $this->createMd5Driver();
+	}
+
+	/**
+	 * Replace the Argon2id hash Driver with MD5 Driver.
+	 *
+	 * @return Md5Hasher
+	 */
+	public function createArgon2idDriver()
+	{
+		return $this->createMd5Driver();
+	}
+
+	/**
+	 * Get the default driver name, always return MD5.
 	 *
 	 * @return string
 	 */
@@ -23,4 +55,17 @@ class Md5HasherManager extends HashManager
 	{
 		return Md5Hasher::ALGO;
 	}
+
+	/**
+	 * Get a driver instance, overwrite to MD5.
+	 *
+	 * @param  string  $driver
+	 * @return mixed
+	 */
+	public function driver($driver = null)
+	{
+		return $this->getDefaultDriver();
+	}
+
+
 }
