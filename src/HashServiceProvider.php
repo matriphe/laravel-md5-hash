95f63e549a12f3ec0d29fbb6f16bce0c2e579471
Muhammad Zamroni <halo@matriphe.com>
2022-10-02 12:31:14 +0200

    Extend from Laravel's HashServiceProvider

--- src/HashServiceProvider.php
+++ src/HashServiceProvider.php
@@ -2,39 +2,12 @@
 
 namespace Matriphe\Md5Hash;
 
-use Illuminate\Support\ServiceProvider;
-
-class HashServiceProvider extends ServiceProvider
+class HashServiceProvider extends \Illuminate\Hashing\HashServiceProvider
 {
-    /**
-     * Indicates if loading of the provider is deferred.
-     *
-     * @var bool
-     */
-    protected $defer = true;
-
-    /**
-     * Register the application services.
-     *
-     */
     public function register()
     {
         $this->app->singleton('hash', function ($app) {
             return new Md5HasherManager($app);
         });
-
-        $this->app->singleton('hash.driver', function ($app) {
-            return $app['hash']->driver();
-        });
-    }
-
-    /**
-     * Get the services provided by the provider.
-     *
-     * @return array
-     */
-    public function provides()
-    {
-        return ['hash'];
     }
 }
